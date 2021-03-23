f4e10394da0ddcec135cb43687f0a1393db22a7f
mbuiakova <maryjein@list.ru>
2021-03-23 21:27:24 +0100

    -change inmemory.xml for UserServiseTest -ignore test createWithException AbstractUserServiceTest for jdbs-repo - add abstract controller - implement JspMealController.java

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -0,0 +1,87 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
+
+@Controller
+public class JspMealController extends AbstractMealController {
+
+    @Autowired
+    public JspMealController(MealService mealService) {
+        super.service = mealService;
+    }
+
+    @GetMapping("/meals")
+    public String get(HttpServletRequest request, Model model) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        String action = request.getParameter("action");
+
+        switch (action == null ? "all" : action) {
+            case "delete" -> {
+                int meal_id = Integer.parseInt(request.getParameter("id"));
+                super.delete(meal_id);
+                return "redirect:meals";
+            }
+            case "update", "create" -> {
+                final Meal meal = "create".equals(action) ?
+                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
+                        super.get(Integer.parseInt(request.getParameter("id")));
+                model.addAttribute("meal", meal);
+                return "/mealForm";
+            }
+            case "filter" -> {
+                LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+                LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+                LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+                LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+                List<MealTo> meals = super.getBetween(startDate, startTime, endDate, endTime);
+//                request.setAttribute("meals", meals);
+                model.addAttribute("meals", meals);
+                return "/meals";
+            }
+            default -> {
+                List<MealTo> meals = super.getAll();
+                //request.setAttribute("meals", meals);
+                model.addAttribute("meals", meals);
+                return "/meals";
+            }
+        }
+    }
+
+    @PostMapping("/meals")
+    public String post(HttpServletRequest request) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+
+        if (StringUtils.hasLength(request.getParameter("id"))) {
+            super.update(meal, Integer.parseInt(request.getParameter("id")));
+        } else {
+            super.create(meal);
+        }
+        return "redirect:meals";
+
+    }
+
+
+}
+
--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -1,9 +1,9 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.DataAccessException;
 import ru.javawebinar.topjava.UserTestData;
 import ru.javawebinar.topjava.model.Role;
@@ -11,9 +11,11 @@ import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import javax.validation.ConstraintViolationException;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
+
 import ru.javawebinar.topjava.repository.JpaUtil;
 
 import static org.junit.Assert.assertThrows;
@@ -27,13 +29,18 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     @Autowired
     private CacheManager cacheManager;
 
-    @Autowired
+    @Autowired(required = false)
     protected JpaUtil jpaUtil;
+
+    @Autowired
+    private Environment environment;
 
     @Before
     public void setup() {
         cacheManager.getCache("users").clear();
-        jpaUtil.clear2ndLevelHibernateCache();
+        if (!Arrays.asList(environment.getActiveProfiles()).contains("jdbc")) {
+            jpaUtil.clear2ndLevelHibernateCache();
+        }
     }
 
     @Test
@@ -95,6 +102,7 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
 
     @Test
     public void createWithException() throws Exception {
+        Assume.assumeFalse(Arrays.asList(environment.getActiveProfiles()).contains("jdbc"));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "  ", "mail@yandex.ru", "password", Role.USER)));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "  ", "password", Role.USER)));
         validateRootCause(ConstraintViolationException.class, () -> service.create(new User(null, "User", "mail@yandex.ru", "  ", Role.USER)));
